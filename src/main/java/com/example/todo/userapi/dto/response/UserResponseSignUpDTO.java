14851fbed085b3c40da222b24d5a34af276b4d29
flatcalm <a_abnc@naver.com>
2023-06-22 11:28:59 +0900

    # 2-1. User API 스펙 구현 중 (UserServiceTest 진행 중)

---
+++ src/main/java/com/example/todo/userapi/dto/response/UserResponseSignUpDTO.java
@@ -0,0 +1,27 @@
+package com.example.todo.userapi.dto.response;
+
+import com.example.todo.userapi.entity.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter @Getter
+@ToString @EqualsAndHashCode(of = "email")
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class UserResponseSignUpDTO {
+
+    private String email;
+
+    private String userName;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime joinDate;
+
+    public UserResponseSignUpDTO(User user) {
+        this.email = user.getEmail();
+        this.userName = user.getUserName();
+        this.joinDate = user.getJoinDate();
+    }
+}
